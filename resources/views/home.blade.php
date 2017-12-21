bf2c8fd999d79c28f4bb16724fc8508843056479
Fabrizio Olivadese <f.olivadese1@campus.unimib.it>
2017-12-21 18:10:09 +0100

    Add Bulma-card for visualize statistics and trips

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,38 +9,49 @@
         <h1>Sei Admin!</h1>
     @endif
 
-    <h2 class="is-size-4">Your trips</h2>
+    <h2 class="is-size-2">Your Trips</h2>
     @forelse(Auth::user()->trips as $trip)
-
-        <div class="box">
-            <code>{{ $trip }}</code>
-            On days: 
-            @foreach($trip->repeatingOn as $day)
-                {{-- This will get the next $day date and only print the day of the week name, "Monday" for example --}}
-                {{ \Carbon\Carbon::now()->next($day)->format('l') }}
-            @endforeach
-            <br><br>     
-            With parts: <br>
-            @forelse($trip->parts as $part)
-                <code>{{ $part }}</code>
-                @if ($part->child_type == "TrenordTripPart")
-                    {{ $part->details->trainId }} 
-                @endif
-            @empty
-                <b>This should never happen</b>
-            @endforelse
+        <div class="card">
+            <div class="card-content">
+                <p class="title">
+                    Trip
+                </p>
+                <code>{{ $trip }}</code>
+                On days: 
+                @foreach($trip->repeatingOn as $day)
+                        {{-- This will get the next $day date and only print the day of the week name, "Monday" for example --}}
+                        {{ \Carbon\Carbon::now()->next($day)->format('l') }}
+                @endforeach
+                <br><br>     
+                With parts: <br>
+                @forelse($trip->parts as $part)
+                    <code>{{ $part }}</code>
+                    @if ($part->child_type == "TrenordTripPart")
+                        {{ $part->details->trainId }} 
+                    @endif
+                @empty
+                    <b>This should never happen</b>                                  
+                @endforelse
         </div>
-    @empty
-        No Trips :(
-    @endforelse
-
-    <h2>Your statistics</h2>
-    @foreach(Auth::user()->personalStatistic as $stats)
-        <code>
-            <b>{{ $stats->year }}/{{ $stats->month }}</b> <br>
-            {{ $stats }} <hr>
-        </code>
-        
+            </div>
         <br>
-    @endforeach
+    @empty
+        No Trips :(    
+    @endforelse
+    
+    <h2 class="is-size-2">Your Statistics</h2>
+            @foreach(Auth::user()->personalStatistic as $stats)
+                 
+                    <div class="card">
+                        <div class="card-content">
+                            <p class="title">
+                                Statistic <code>  {{$stats->month}}/{{$stats->year}} </code>
+                            </p>
+                            <p class="subtitle">
+                                <code>{{$stats}}</code>
+                            </p>
+                        </div>
+                    </div>
+                    <br>
+            @endforeach
 @endsection
