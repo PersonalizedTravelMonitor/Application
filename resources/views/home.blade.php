549cf39fd24698f2d8811863e443ea2f1535fa1f
Fabrizio Olivadese <f.olivadese1@campus.unimib.it>
2017-12-21 12:05:25 +0100

    Use Bulma for style

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,62 +1,46 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+    You are logged in!
+    {{ Auth::user()->email }} <br>
 
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+    {{-- Auth::user() is the logged-in user --}}
+    @if (Auth::user()->isAdmin)
+        <h1>Sei Admin!</h1>
+    @endif
 
-                    You are logged in!
-                    {{ Auth::user()->email }} <br>
+    <h2 class="is-size-4">Your trips</h2>
+    @forelse(Auth::user()->trips as $trip)
 
-                    {{-- Auth::user() is the logged-in user --}}
-                    @if (Auth::user()->isAdmin)
-                        <h1>Sei Admin!</h1>
-                    @endif
-                    
-                    <h2>Your trips</h2>
-                    @forelse(Auth::user()->trips as $trip)
-                        <code>{{ $trip }}</code>
-                        On days: 
-                        @foreach($trip->repeatingOn as $day)
-                            {{-- This will get the next $day date and only print the day of the week name, "Monday" for example --}}
-                            {{ \Carbon\Carbon::now()->next($day)->format('l') }}
-                        @endforeach
-                        <br><br>     
-                        With parts: <br>
-                        @forelse($trip->parts as $part)
-                            <code>{{ $part }}</code>
-                            @if ($part->child_type == "TrenordTripPart")
-                                {{ $part->details->trainId }} 
-                            @endif
-                        @empty
-                            <b>This should never happen</b>
-                        @endforelse
-                        <hr>
-                    @empty
-                        No Trips :(
-                    @endforelse
-
-                    <h2>Your statistics</h2>
-                    @foreach(Auth::user()->personalStatistic as $stats)
-                        <code>
-                           <b>{{ $stats->year }}/{{ $stats->month }}</b> <br>
-                            {{ $stats }} <hr>
-                        </code>
-                        
-                        <br>
-                    @endforeach
-                </div>
-            </div>
+        <div class="box">
+            <code>{{ $trip }}</code>
+            On days: 
+            @foreach($trip->repeatingOn as $day)
+                {{-- This will get the next $day date and only print the day of the week name, "Monday" for example --}}
+                {{ \Carbon\Carbon::now()->next($day)->format('l') }}
+            @endforeach
+            <br><br>     
+            With parts: <br>
+            @forelse($trip->parts as $part)
+                <code>{{ $part }}</code>
+                @if ($part->child_type == "TrenordTripPart")
+                    {{ $part->details->trainId }} 
+                @endif
+            @empty
+                <b>This should never happen</b>
+            @endforelse
         </div>
-    </div>
-</div>
+    @empty
+        No Trips :(
+    @endforelse
+
+    <h2>Your statistics</h2>
+    @foreach(Auth::user()->personalStatistic as $stats)
+        <code>
+            <b>{{ $stats->year }}/{{ $stats->month }}</b> <br>
+            {{ $stats }} <hr>
+        </code>
+        
+        <br>
+    @endforeach
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,70 +11,31 @@
     <title>{{ config('app.name', 'PTM') }}</title>
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.1/css/bulma.min.css">
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-default navbar-static-top">
-            <div class="container">
-                <div class="navbar-header">
+    <nav class="navbar is-fixed-top" role="navigation" aria-label="main navigation">
+        <div class="navbar-brand">
+            <a class="navbar-item" href="https://bulma.io">
+                PTM
+            </a>
 
-                    <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse" aria-expanded="false">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-
-                    <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'PTM') }}
-                    </a>
-                </div>
-
-                <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-                        &nbsp;
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="nav navbar-nav navbar-right">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li><a href="{{ route('login') }}">Login</a></li>
-                            <li><a href="{{ route('register') }}">Register</a></li>
-                        @else
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <ul class="dropdown-menu">
-                                    <li>
-                                        <a href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            Logout
-                                        </a>
-
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                </ul>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
+            <button class="button navbar-burger">
+                <span></span>
+                <span></span>
+                <span></span>
+            </button>
+        </div>
+    </nav>
+    <section class="section">
+        <div class="container">
+            <h1 class="title">PTM</h1>
+            <p class="subtitle">Your personal travel monitor!</p>
+            <div id="app">
+                @yield('content')
             </div>
-        </nav>
-
-        @yield('content')
-    </div>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}"></script>
+        </div>
+    </section>
 </body>
 </html>
