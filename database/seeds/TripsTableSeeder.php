c6aa17ce519749db045612e927d968554e4b4834
Cristian Baldi <c.baldi1@campus.unimib.it>
2017-12-21 18:27:25 +0000

    Remove useless trip from TripSeeder (no subtrips)

--- database/seeds/TripsTableSeeder.php
+++ database/seeds/TripsTableSeeder.php
@@ -16,9 +16,9 @@ class TripsTableSeeder extends Seeder
             'repeatingOn' => json_encode([Carbon::MONDAY, Carbon::TUESDAY]),
             'user_id' => App\User::where('name', 'traveler1')->first()->id
         ]);
-        DB::table('trips')->insert([
+        /*DB::table('trips')->insert([
             'repeatingOn' => json_encode([Carbon::SATURDAY, Carbon::SUNDAY]),
             'user_id' => App\User::where('name', 'traveler1')->first()->id
-        ]);
+        ]);*/
     }
 }
