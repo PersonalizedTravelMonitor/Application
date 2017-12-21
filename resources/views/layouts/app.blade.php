b58f7cfe4d4e663fb1e0354401be08bf6cdc1580
Fabrizio Olivadese <f.olivadese1@campus.unimib.it>
2017-12-21 18:09:12 +0100

    Add new entry to navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,8 +17,11 @@
 <body>
     <nav class="navbar is-fixed-top" role="navigation" aria-label="main navigation">
         <div class="navbar-brand">
-            <a class="navbar-item" href="https://bulma.io">
-                PTM
+            <a class="navbar-item" href="/">
+                Home
+            </a>
+            <a class="navbar-item" href="https://github.com/PersonalizedTravelMonitor" target="_blank">
+                GhitHub Repo
             </a>
 
             <button class="button navbar-burger">
