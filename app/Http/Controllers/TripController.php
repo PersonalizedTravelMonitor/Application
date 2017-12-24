9fc850e50a7dfa3cfc0e3d42bfe5ba9fefb38f67
Fabrizio Olivadese <f.olivadese1@campus.unimib.it>
2017-12-24 16:44:05 +0100

    Add sample TripController

---
+++ app/Http/Controllers/TripController.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Trip;
+use App\TrenordTripPart;
+use App\TripPart;
+use Auth;
+use Illuminate\Http\Request;
+
+class TripController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Trip::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        $trip = new Trip;
+        $trip->user_id = Auth::user()->id;
+        $trip->repeatingOn = [3,4];
+        $trip->save();
+
+        $trenordTripPart = new TrenordTripPart;
+        $trenordTripPart->line= "S8";
+        $trenordTripPart->departure="07:37:00";
+        $trenordTripPart->arrival="08:23:00";
+        $trenordTripPart->trainId="10845";
+        $trenordTripPart->save();
+
+        $tripPart = new TripPart; // Concreto
+        $tripPart->from = "Lecco";
+        $tripPart->to = "Milano Greco Pirelli";
+        $tripPart->child_id = $trenordTripPart->id;
+        $tripPart->child_type = "TrenordTripPart";
+        $tripPart->trip_id = $trip->id;
+        $tripPart->save();
+
+        return "ok";
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Trip  $trip
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Trip $trip)
+    {
+        return $trip;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Trip  $trip
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Trip $trip)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Trip  $trip
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Trip $trip)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Trip  $trip
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Trip $trip)
+    {
+        //
+    }
+}
