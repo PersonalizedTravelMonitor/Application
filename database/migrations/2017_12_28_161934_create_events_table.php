52ae7ff33bbf6d5011ee2c78d888ed258d0437b1
Fabrizio Olivadese <f.olivadese1@campus.unimib.it>
2017-12-28 17:23:46 +0100

    Add Event model

---
+++ database/migrations/2017_12_28_161934_create_events_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+
+            $table->string('severity');  //TO DO: creare il tipo SeverityLevel
+            $table->time('createdAt');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
