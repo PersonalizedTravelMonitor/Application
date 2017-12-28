850a4b29474bd3daaed73f2f5e508ad9a4790e22
Simone Galimberti <s.galimberti32@campus.unimib.it>
2017-12-28 16:02:41 +0100

    Add TrenitaliaTripPart model

---
+++ app/TrenitaliaTripPart.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TrenitaliaTripPart extends Model
+{
+     // TODO
+    /* public function trip() {
+
+    } */
+
+    public function tripPart()
+    {
+        return $this->morphOne('TripPart', 'details');
+    }
+}
---
+++ database/migrations/2017_12_28_145751_create_trenitalia_trip_parts_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTrenitaliaTripPartsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trenitalia_trip_parts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+
+            $table->time('departure');
+            $table->time('arrival');
+
+            // the id of the train to follow, eg: 10846
+            $table->string('trainId');
+            // the platform used at the departure station
+            $table->string('platform'); 
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trenitalia_trip_parts');
+    }
+}
