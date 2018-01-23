3f5a7082fc916f779ea88e40709093a98ee1aae1
Fabrizio Olivadese <f.olivadese1@campus.unimib.it>
2018-01-23 16:53:12 +0100

    Add createTrain view

---
+++ resources/views/trips/createTrain.blade.php
@@ -0,0 +1,102 @@
+<div class="columns">
+    <div class="column">
+      <div class="field">
+        <label class="label">From: </label>
+        <div class="control has-icons-left">
+          <input class="input" type="text" placeholder="Departure Station Name">
+          <span class="icon is-small is-left">
+          <i class="fas fa-map-marker"></i> </span>
+        </div>
+      </div>
+    </div>
+    <div class="column">
+      <div class="field">
+        <label class="label">To: </label>
+        <div class="control has-icons-left">
+          <input class="input" type="text" placeholder="Arrival Station Name">
+          <span class="icon is-small is-left">
+          <i class="fas fa-map-marker"></i> </span>
+        </div>
+      </div>
+    </div>
+    <div class="column">
+      <div class="field">
+        <label class="label">Time: </label>
+        <div class="control has-icons-left">
+          <input class="input" type="text" placeholder="Departure Time">
+          <span class="icon is-small is-left">
+          <i class="fas fa-clock"></i> </span>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="columns">
+    <div class="column">
+      <div class="card">
+      <header class="card-header">
+        <p class="card-header-title">
+          Treno 5040  -  From Lecco [12:00] To Rome [18:00]
+        </p>
+        <a href="#" class="card-header-icon" aria-label="more options">
+          <span class="icon">
+            <i class="fas fa-angle-down" aria-hidden="true"></i>
+          </span>
+        </a>
+      </header>
+      <div class="card-content">
+        <div class="content">
+        <div class="columns">
+        <div class="column">
+          <b>Durata:</b> 5h 25m<br>
+                <b>Compagnia:</b> Trenitalia<br>
+                <b>Tipo di Viaggio:</b> Diretto<br>
+                <br>
+        </div>
+        <div class="column">
+          <img src="https://pbs.twimg.com/profile_images/1620236068/TRENORD_LOGO.jpg" width="80px" height="80px" align="right">
+        </div>
+        </div>
+        </div>
+      </div>
+      <footer class="card-footer">
+        <a href="#" class="card-footer-item">Più informazioni</a>
+        <a href="#" class="card-footer-item">Seleziona</a>
+      </footer>
+    </div>
+    </div>
+    <div class="column">
+    <div class="card">
+      <header class="card-header">
+        <p class="card-header-title">
+          Treno 5040  -  From Naples [18:00] To Reggio Calabria [22:00]
+        </p>
+        <a href="#" class="card-header-icon" aria-label="more options">
+          <span class="icon">
+            <i class="fas fa-angle-down" aria-hidden="true"></i>
+          </span>
+        </a>
+      </header>
+      <div class="card-content">
+        <div class="content">
+        <div class="columns">
+        <div class="column">
+          <b>Durata:</b> 5h 25m<br>
+          <b>Compagnia:</b> Trenord<br>
+          <b>Tipo di Viaggio:</b> Diretto<br>
+          <br>
+        </div>
+        <div class="column">
+          <img src="https://cdn.logitravel.it/comun/images/trenes/companias/logos/logo_TIT.png" width="80px" height="80px" align="right">
+        </div>
+        </div>
+        </div>
+        </div>
+      <footer class="card-footer">
+        <a href="#" class="card-footer-item">Più informazioni</a>
+        <a href="#" class="card-footer-item">Seleziona</a>
+      </footer>
+      </div>
+      </div>
+    </div>
+  </div>
