c2c110a52dd558cf65e1588d276edbb88074f556
Fabrizio Olivadese <f.olivadese1@campus.unimib.it>
2018-01-23 16:54:05 +0100

    Add include to new views

--- resources/views/trips/create.blade.php
+++ resources/views/trips/create.blade.php
@@ -35,148 +35,46 @@
   </ul>
 </div>
 <div class="tab tab--train">
-  <div class="columns">
-    <div class="column">
-      <div class="field">
-        <label class="label">From: </label>
-        <div class="control has-icons-left">
-          <input class="input" type="text" placeholder="Departure Station Name">
-          <span class="icon is-small is-left">
-          <i class="fas fa-map-marker"></i> </span>
-        </div>
-      </div>
-    </div>
-    <div class="column">
-      <div class="field">
-        <label class="label">To: </label>
-        <div class="control has-icons-left">
-          <input class="input" type="text" placeholder="Arrival Station Name">
-          <span class="icon is-small is-left">
-          <i class="fas fa-map-marker"></i> </span>
-        </div>
-      </div>
-    </div>
-    <div class="column">
-      <div class="field">
-        <label class="label">Time: </label>
-        <div class="control has-icons-left">
-          <input class="input" type="text" placeholder="Departure Time">
-          <span class="icon is-small is-left">
-          <i class="fas fa-clock"></i> </span>
-        </div>
-      </div>
-    </div>
-  </div>
-
-  <div class="columns">
-    <div class="column">
-      <div class="card">
-      <header class="card-header">
-        <p class="card-header-title">
-          Treno 5040  -  From Lecco [12:00] To Rome [18:00]
-        </p>
-        <a href="#" class="card-header-icon" aria-label="more options">
-          <span class="icon">
-            <i class="fas fa-angle-down" aria-hidden="true"></i>
-          </span>
-        </a>
-      </header>
-      <div class="card-content">
-        <div class="content">
-        <div class="columns">
-        <div class="column">
-          <b>Durata:</b> 5h 25m<br>
-                <b>Compagnia:</b> Trenitalia<br>
-                <b>Tipo di Viaggio:</b> Diretto<br>
-                <br>
-        </div>
-        <div class="column">
-          <img src="https://pbs.twimg.com/profile_images/1620236068/TRENORD_LOGO.jpg" width="80px" height="80px" align="right">
-        </div>
-        </div>
-        </div>
-      </div>
-      <footer class="card-footer">
-        <a href="#" class="card-footer-item">Più informazioni</a>
-        <a href="#" class="card-footer-item">Seleziona</a>
-      </footer>
-    </div>
-    </div>
-    <div class="column">
-    <div class="card">
-      <header class="card-header">
-        <p class="card-header-title">
-          Treno 5040  -  From Naples [18:00] To Reggio Calabria [22:00]
-        </p>
-        <a href="#" class="card-header-icon" aria-label="more options">
-          <span class="icon">
-            <i class="fas fa-angle-down" aria-hidden="true"></i>
-          </span>
-        </a>
-      </header>
-      <div class="card-content">
-        <div class="content">
-        <div class="columns">
-        <div class="column">
-          <b>Durata:</b> 5h 25m<br>
-          <b>Compagnia:</b> Trenord<br>
-          <b>Tipo di Viaggio:</b> Diretto<br>
-          <br>
-        </div>
-        <div class="column">
-          <img src="https://cdn.logitravel.it/comun/images/trenes/companias/logos/logo_TIT.png" width="80px" height="80px" align="right">
-        </div>
-        </div>
-        </div>
-        </div>
-      <footer class="card-footer">
-        <a href="#" class="card-footer-item">Più informazioni</a>
-        <a href="#" class="card-footer-item">Seleziona</a>
-      </footer>
-      </div>
-      </div>
-    </div>
-  </div>
-  </div>
-  <br>
-  <h2 class="title is-5" align="center">Select the days</h2>
-  <div class="columns is-centered">
-  <div class="column is-narrow">
-  <a class="button is-rounded is-info is-selected">Lunedì</a>
-  </div>
-  <div class="column is-narrow">
-  <a class="button is-rounded">Martedì</a>
-  </div>
-  <div class="column is-narrow">
-  <a class="button is-rounded">Mercoledì</a>
-  </div>
-  <div class="column is-narrow">
-  <a class="button is-rounded">Giovedì</a>
-  </div>
-  <div class="column is-narrow">
-  <a class="button is-rounded">Venerdì</a>
-  </div>
-  <div class="column is-narrow">
-  <a class="button is-rounded">Sabato</a>
-  </div>
-  <div class="column is-narrow">
-  <a class="button is-rounded">Domenica</a>
-  </div>
-</div>
-
-<div class="buttons is-right">
-  <span class="button is-danger">Cancel</span>
-  <span class="button is-success">Submit</span>
-</div>
-
-
+  @include('trips.createTrain')
 </div>
 <div class="tab tab--subway" style="display:none">
-<h1>Sono il subway</h1></div>
+  @include('trips.createSubway')
 <div class="tab tab--bus" style="display:none">
-<h1>Sono il bus</h1></div>
+  @include('trips.createBus')
 <div class="tab tab--tram" style="display:none">
-<h1>Sono il tram</h1>
+  @include('trips.createTram')
+</div>
+</div>
+
+<br>
+<h2 class="title is-5" align="center">Select the days</h2>
+<div class="columns is-centered">
+  <div class="column is-narrow">
+    <a class="button is-rounded is-info is-selected">Lunedì</a>
+  </div>
+  <div class="column is-narrow">
+    <a class="button is-rounded">Martedì</a>
+  </div>
+  <div class="column is-narrow">
+    <a class="button is-rounded">Mercoledì</a>
+  </div>
+  <div class="column is-narrow">
+    <a class="button is-rounded">Giovedì</a>
+  </div>
+  <div class="column is-narrow">
+    <a class="button is-rounded">Venerdì</a>
+  </div>
+  <div class="column is-narrow">
+    <a class="button is-rounded">Sabato</a>
+  </div>
+  <div class="column is-narrow">
+    <a class="button is-rounded">Domenica</a>
+  </div>
+</div>
+  <br>
+  <div class="buttons is-right">
+  <span class="button is-danger">Cancel</span>
+  <span class="button is-success">Submit</span>
 </div>
 
 
