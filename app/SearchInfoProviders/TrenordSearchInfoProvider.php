671c04e444fc940f555ef3907e54b8de80a2bf97
Cristian Baldi <c.baldi1@campus.unimib.it>
2018-01-25 10:25:19 +0000

    Add methods for cleaning up search results
    TODO: Extract class because class is too large

--- app/SearchInfoProviders/TrenordSearchInfoProvider.php
+++ app/SearchInfoProviders/TrenordSearchInfoProvider.php
@@ -7,6 +7,15 @@ use Carbon\Carbon;
 use DateTimeZone;
 use App\ExternalAPIs\TrenordAPI;
 
+function renameKey($array, $oldkey, $newkey) {
+    if (!isset($array[$oldkey])) return $array;
+
+    $array[$newkey] = $array[$oldkey];
+    unset($array[$oldkey]);
+
+    return $array;
+}
+
 class TrenordSearchInfoProvider implements SearchInfoProvider
 {
     public static function autocompleteFrom($partialFrom) {
@@ -22,9 +31,12 @@ class TrenordSearchInfoProvider implements SearchInfoProvider
     }
 
     public static function searchSolutions($from, $to, $hour) {
-        //dd(Carbon::now());
-        return TrenordAPI::search($from,$to,Carbon::createFromTime($hour, 0, 0, 'Europe/Rome'));
-        
+        $searchResults = TrenordAPI::search($from,$to,Carbon::createFromTime($hour, 0, 0, 'Europe/Rome'));
+
+        $cleanedupResults = self::cleanupSearchResults($searchResults);
+
+        return response()->json($cleanedupResults);
+
     }
 
     static function filterStations($stations, $partial) {
@@ -35,7 +47,82 @@ class TrenordSearchInfoProvider implements SearchInfoProvider
         }));
     }
 
-    static function 
+    static function cleanupSearchResults($searchResults) {
+        foreach ($searchResults as $key => $trip) {
+            $searchResults[$key] = self::cleanupTripResult($trip);
+        }
+
+        dd($searchResults);
+    }
+
+    static function cleanupTripResult($trip) {
+        $trip = renameKey($trip, "dep_time", "departure_time");
+        $trip = renameKey($trip, "arr_time", "arrival_time");
+
+        unset($trip["walk"]);
+        unset($trip["bicycle"]);
+        unset($trip["handicap"]);
+        unset($trip["class_1"]);
+        unset($trip["class_2"]);
+        unset($trip["class_1_and_2"]);
+        unset($trip["mxp"]);
+        unset($trip["mxp_special"]);
+        unset($trip["dep_day_offset"]);
+        unset($trip["arr_day_offset"]);
+        unset($trip["price"]);
+        unset($trip["price"]);
+
+        unset($trip["dep_station"]["station_externalId"]);
+        unset($trip["dep_station"]["station_externalStationNr"]);
+        unset($trip["dep_station"]["station_type"]);
+        $trip["dep_station"] = renameKey($trip["dep_station"], "station_ori_name", "station_name");
+        $trip = renameKey($trip, "dep_station", "departure_station");
+
+        unset($trip["arr_station"]["station_externalId"]);
+        unset($trip["arr_station"]["station_externalStationNr"]);
+        unset($trip["arr_station"]["station_type"]);
+        $trip["arr_station"] = renameKey($trip["arr_station"], "station_ori_name", "station_name");
+        $trip = renameKey($trip, "arr_station", "arrival_station");
+
+        foreach ($trip["journey_list"] as $key => $tripPart) {
+            $tripPart = self::cleanupTripPartResult($tripPart);
+
+            $trip["journey_list"][$key] = $tripPart;
+        }
+        return $trip;
+    }
+
+    static function cleanupTripPartResult($tripPart) {
+        $train = [];
+        $train["train_name"] = $tripPart["train"]["train_name"];
+        $train["train_category"] = $tripPart["train"]["train_category"];
+        $train["direction"] = $tripPart["train"]["direction"];
+        $train["line"] = $tripPart["train"]["direttrice"];
+        $tripPart["train"] = $train;
+        $tripPart = renameKey($tripPart, "pass_list", "stops");
+
+        foreach ($tripPart["stops"] as $key => $stop) {
+            $stop = self::cleanupStopResult($stop);
+            $tripPart["stops"][$key] = $stop;
+        }
+        return $tripPart;
+    }
+
+    static function cleanupStopResult($stop) {
+        $stop = renameKey($stop, "arr_time", "arrival_time");
+        $stop = renameKey($stop, "dep_time", "departure_time");
+        unset($stop["is_journey"]);
+        unset($stop["actual_data"]);
+        unset($stop["arr_day_offset"]);
+        unset($stop["dep_day_offset"]);
+
+        unset($stop["station"]["station_externalId"]);
+        unset($stop["station"]["station_externalStationNr"]);
+        unset($stop["station"]["station_type"]);
+        $stop["station"] = renameKey($stop["station"], "station_ori_name", "station_name");
+
+        return $stop;
+    }
 
     static function parseStationResponse($body) {
         $body = trim($body);
