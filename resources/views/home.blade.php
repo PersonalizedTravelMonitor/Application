22f0bf99acbe4edbf7387c0d407eeff8ab186944
Cristian Baldi <c.baldi1@campus.unimib.it>
2018-01-27 22:03:46 +0100

    Start implementing real time push notification

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,6 +8,8 @@
     <h1>You are Admin!</h1>
   @endif
 
+  <button class="button" onclick="enableNotifications()">Register for notifications</button>
+
   <h2 class="is-size-2">Your Trips</h2>
   <a class="is-size-4" href="{{ route('trips.create') }}">Create a trip</a>
   <br>
@@ -115,4 +117,119 @@
   @empty
     <p>No recent announcements from the Admins to show</p>
   @endforelse
+@endsection
+
+@section('scripts')
+<script>
+var _registration = null;
+function registerServiceWorker() {
+  return navigator.serviceWorker.register('js/service-worker.js')
+  .then(function(registration) {
+    console.log('Service worker successfully registered.');
+    _registration = registration;
+    return registration;
+  })
+  .catch(function(err) {
+    console.error('Unable to register service worker.', err);
+  });
+}
+
+function askPermission() {
+  return new Promise(function(resolve, reject) {
+    const permissionResult = Notification.requestPermission(function(result) {
+      resolve(result);
+    });
+
+    if (permissionResult) {
+      permissionResult.then(resolve, reject);
+    }
+  })
+  .then(function(permissionResult) {
+    if (permissionResult !== 'granted') {
+      throw new Error('We weren\'t granted permission.');
+    }
+    else{
+      subscribeUserToPush();
+    }
+  });
+}
+
+function urlBase64ToUint8Array(base64String) {
+  const padding = '='.repeat((4 - base64String.length % 4) % 4);
+  const base64 = (base64String + padding)
+    .replace(/\-/g, '+')
+    .replace(/_/g, '/');
+
+  const rawData = window.atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
+}
+
+function getSWRegistration(){
+  var promise = new Promise(function(resolve, reject) {
+  // do a thing, possibly async, then…
+
+  if (_registration != null) {
+    resolve(_registration);
+  }
+  else {
+    reject(Error("It broke"));
+  }
+  });
+  return promise;
+}
+
+function subscribeUserToPush() {
+  getSWRegistration()
+  .then(function(registration) {
+    console.log(registration);
+    const subscribeOptions = {
+      userVisibleOnly: true,
+      applicationServerKey: urlBase64ToUint8Array(
+        "{{env('VAPID_PUBLIC_KEY')}}"
+      )
+    };
+
+    return registration.pushManager.subscribe(subscribeOptions);
+  })
+  .then(function(pushSubscription) {
+    console.log('Received PushSubscription: ', JSON.stringify(pushSubscription));
+    sendSubscriptionToBackEnd(pushSubscription);
+    return pushSubscription;
+  });
+}
+
+function sendSubscriptionToBackEnd(subscription) {
+  return fetch('/api/save-subscription/{{Auth::user()->id}}', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(subscription)
+  })
+  .then(function(response) {
+    if (!response.ok) {
+      throw new Error('Bad status code from server.');
+    }
+
+    return response.json();
+  })
+  .then(function(responseData) {
+    if (!(responseData && responseData.success)) {
+      throw new Error('Bad response from server.');
+    }
+  });
+}
+
+function enableNotifications(){
+  //register service worker
+  //check permission for notification/ask
+  askPermission();
+}
+registerServiceWorker();
+</script>
 @endsection
