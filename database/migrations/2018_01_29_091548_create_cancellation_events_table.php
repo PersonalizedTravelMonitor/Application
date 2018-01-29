77496d8fad31ac36014618ab7217390812495162
Cristian Baldi <c.baldi1@campus.unimib.it>
2018-01-29 09:20:16 +0000

    Add migration for cancellation events

---
+++ database/migrations/2018_01_29_091548_create_cancellation_events_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCancellationEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cancellation_events', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+
+            $table->string('cause')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cancelation_events');
+    }
+}
