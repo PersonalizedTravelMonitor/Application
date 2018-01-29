5c83015864fc76e79e00f7ab2450f56ffbdf15e3
Cristian Baldi <c.baldi1@campus.unimib.it>
2018-01-29 09:30:12 +0000

    Add support for cancellation events in TrenordTripPartManager

--- app/TripPartManagers/TrenordTripPartManager.php
+++ app/TripPartManagers/TrenordTripPartManager.php
@@ -6,6 +6,7 @@ use App\TripPart;
 use App\Event;
 use App\TravelerReportEvent;
 use App\DelayEvent;
+use App\CancellationEvent;
 use App\ExternalAPIs\TrenordAPI;
 use Log;
 
@@ -13,17 +14,30 @@ class TrenordTripPartManager implements TripPartManager
 {
     public static function getEvents(TripPart $tripPart)
     {
-        // chiedi le info a trenord
         $info = TrenordAPI::getTrainInfo($tripPart->details->trainId);
-        // capisci cosa vogliono dire
-        var_dump($info);
         $info = $info[0];
+
+        if($info["cancelled"]) {
+            $event = new CancellationEvent;
+            $event->save();
+
+            $parentEvent = new Event;
+            $parentEvent->trip_part_id = $tripPart->id;
+            $parentEvent->details_id = $event->id;
+            $parentEvent->details_type = get_class($event);
+            $parentEvent->severity = "CRITICAL";
+            $parentEvent->save();
+
+            echo "Adding cancellation event for tripPart " . $tripPart->id;
+
+            $tripPart->is_checked = true;
+            $tripPart->save();
+            return;
+        }
 
         $delay = $info["delay"];
         $train = $info["journey_list"][0]["train"];
-
         $status = $train["status"];
-
         if($status == "N") {
             // deve ancora partire
         } else if ($status == "V") {
@@ -47,8 +61,10 @@ class TrenordTripPartManager implements TripPartManager
             $parentEvent->trip_part_id = $tripPart->id;
             $parentEvent->details_id = $event->id;
             $parentEvent->details_type = get_class($event);
-            $parentEvent->severity = "INFO";
+            $parentEvent->severity = "WARN";
             $parentEvent->save();
+
+            echo "Adding delay event for tripPart " . $tripPart->id;
         } else if ($status == "A") {
             $tripPart->is_checked = true;
             $tripPart->save();
@@ -64,6 +80,8 @@ class TrenordTripPartManager implements TripPartManager
             $parentEvent->details_type = get_class($event);
             $parentEvent->severity = "INFO";
             $parentEvent->save();
+
+            echo "Adding arrival event for tripPart " . $tripPart->id;
         }
     }
 }
