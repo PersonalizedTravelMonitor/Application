02a3c2bb2065fc8cc3ae395acb175af3da5e41d2
Cristian Baldi <c.baldi1@campus.unimib.it>
2018-01-31 10:04:17 +0100

    Improve code style and decrese code smells

---
+++ app/Utils.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+class Utils {
+
+    public static function renameKey($array, $oldkey, $newkey) {
+        if (!isset($array[$oldkey])) return $array;
+
+        $array[$newkey] = $array[$oldkey];
+        unset($array[$oldkey]);
+
+        return $array;
+    }
+
+}
