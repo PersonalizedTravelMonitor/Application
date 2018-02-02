b08d6e67429bd97588b35f34ce06de8419b83a44
Simone Galimberti <s.galimberti32@campus.unimib.it>
2018-02-02 11:05:23 +0100

    Fix a binding problem within routes about the travelReportEvent

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::get('/auth/{provider}/callback', 'Auth\LoginController@handleProviderCall
 Route::resource('trips','TripController');
 
 Route::post('tripParts/{trip}/{tripPart}/travelerReportEvent','UserReportController@addTravelerReportEvent')->name('tripParts.addTravelerReportEvent');
-Route::get('tripParts/{trip}/{tripPart}/travelerReportEvent/{reportEvent}/delete','UserReportController@removeTravelerReportEvent')->name('tripParts.removeTravelerReportEvent');
+Route::get('trips/{trip}/tripParts/{tripPart}/travelerReportEvent/{event}/delete','UserReportController@removeTravelerReportEvent')->name('tripParts.removeTravelerReportEvent');
 
 Route::prefix('search')->name('search.')->group(function () {
     Route::get('/{infoSource}/autocompleteFrom', 'SearchInfoController@autocompleteFrom')->name('autocompleteFrom');
